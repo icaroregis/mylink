1b4f52c01ee7c0fb4e9f6c6278d766a3c6cd56c9
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-05-31 23:12:45 -0300

    estilizando leftbar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Routes from './src/routes';
+import 'react-native-gesture-handler';
+import Drawer from './src/routes';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
+      <Drawer />
     </NavigationContainer>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,49 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './pages/Home';
 import MyLinks from './pages/MyLinks';
-
-const Drawer = createDrawerNavigator();
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Routes() {
+  const Drawer = createDrawerNavigator();
   return (
-    <Drawer.Navigation>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="MyLinks" component={MyLinks} />
-    </Drawer.Navigation>
+    <Drawer.Navigator
+      drawerContentOptions={{
+        activeBackgroundColor: '#2ccbb9',
+        activeTintColor: '#fff',
+        marginTop: 16,
+        labelStyle: {
+          fontSize: 20,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Encurtar Link',
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? 'cube' : 'cube-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="MyLinks"
+        component={MyLinks}
+        options={{
+          title: 'Meus Links',
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? 'stats-chart' : 'stats-chart-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
   );
 }
