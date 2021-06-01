7a6da3d6f37e930832e2a50be394983c7572b3ea
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-05-31 23:23:58 -0300

    aplicando degrade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Home() {
   return (
-    <View>
+    <LinearGradient
+      colors={['#1ddbb9', '#132742']}
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
       <Text>Página Home</Text>
-    </View>
+    </LinearGradient>
   );
 }
