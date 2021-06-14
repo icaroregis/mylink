df633f302325782b90754731efdfeeaf1639fa4b
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-06-14 00:15:41 -0300

    ...

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -6,7 +6,7 @@ import { ContainerButton, Item } from './styles';
 export default function ListItem() {
   return (
     <View>
-      <ContainerButton>
+      <ContainerButton activeOpacity={0.9} onPress={() => {}}>
         <Feather name="link" color="#fff" size={24} />
         <Item numberOfLines={1}>https://youtube.com</Item>
       </ContainerButton>
