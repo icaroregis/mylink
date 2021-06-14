00cb72e8fa2099bde9716aa018bb04559e478c9c
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-06-13 23:56:00 -0300

    ...

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { ContainerButton, Item } from './styles';
 
 export default function ListItem() {
   return (
     <View>
-      <Text>https://seulink.com</Text>
+      <ContainerButton>
+        <Feather name="link" color="#fff" size={24} />
+        <Item>https://youtube.com</Item>
+      </ContainerButton>
     </View>
   );
 }
---
+++ src/components/ListItem/styles.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components/native';
+
+export const ContainerButton = styled.TouchableOpacity``;
+export const Item = styled.Text``;
