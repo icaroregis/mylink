7a07b9ca9ef624111a87295d2ebe25e144d2c6c6
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2021-07-01 00:00:06 -0300

    ...

--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -50,7 +50,7 @@ export default function MyLinks() {
           <ActivityIndicator color="#fff" />
         </ContainerEmpty>
       }
-      {!loading && links.length === 0 && (
+      {links.length === 0 && (
         <ContainerEmpty>
           <WarningText>Você ainda não possui nenhum link salvo.</WarningText>
         </ContainerEmpty>
